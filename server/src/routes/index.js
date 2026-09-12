c1775af859063b295beea0fd1652b4e9d7c7ea62
florentino <florenpinto000@gmail.com>
2026-09-12 13:16:16 +0900

    reorganize code sequence

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,12 +2,12 @@ import { Router } from "express";
 import authRoutes from "./auth.routes.js";
 import { requireAuth } from "../middleware/auth.middleware.js";
 import { attachProfile } from "../middleware/attachProfile.middleware.js";
+import taskRoutes from "./task.routes.js";
 
 const router = Router();
 
 router.use("/auth", requireAuth, attachProfile, authRoutes);
 
-import taskRoutes from "./task.routes.js";
 router.use("/tasks", requireAuth, attachProfile, taskRoutes);
 
 export default router;
